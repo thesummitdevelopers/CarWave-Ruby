0c5006a38d5502d48f4ca05c6ac5c6d333d84f6c
Ares-Fel <angel.flores.b@tecsup.edu.pe>
2018-06-28 21:31:58 -0500

    Modelo vista controlador de CarWave

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+    get 'home/login'
+    get 'home/registro'
+    #resources :bienvenida do
+    #    collection do
+    #        get :login
+    #    end
+    #end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
